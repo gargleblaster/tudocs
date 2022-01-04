64257955587c73cb6bf8b6ad101dd040610587ac
leeatnomi <67289884+leeatnomi@users.noreply.github.com>
2022-01-03 22:26:47 -0500

    Docusaurus base project

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+const siteConfig = {
+  title: 'Trade Unafraid Documentation', // Title for your website.
+  tagline: 'Precision Trading on Autopilot',
+  url: 'https://docs.tradeunafraid.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // Used for publishing and more
+  projectName: 'trade-unafraid-documentation'
+// ...
+}
